42c030ffa6be2ee8c5d6e7b6040b31cb726f16ba
KvDevX <kvdex9258@gmail.com>
2025-05-02 06:17:29 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -73432,8 +73432,8 @@ gamesse:AddButton({
 	Title = "FE Crash/Lag Sount Bronx",
 	Description = ":)",
 	Callback = function()
-		if game.PlaceId ~= 189707 then
-            game:GetService("TeleportService"):Teleport(189707,game:GetService("Players").LocalPlayer)
+		if game.PlaceId ~= 10179538382 then
+            game:GetService("TeleportService"):Teleport(10179538382,game:GetService("Players").LocalPlayer)
         else
             game:GetService("RunService").RenderStepped:Connect(function()
                 local args = {
